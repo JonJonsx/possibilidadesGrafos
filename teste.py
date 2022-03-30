42a33ae0ae8e1c59bdc2a70113ed6b1406b9aebd
JonJonsx <jonatan.souza@bandtec.com.br>
2022-03-29 21:22:00 -0300

    update project

--- teste.py
+++ teste.py
@@ -1,28 +1,39 @@
 from itertools import product,tee
 
-def verifyValues(value: list):
-    if value == ['A', 'A'] or value == ['B', 'B'] or value == ['C', 'C'] or value == ['D', 'D'] or value == ['E', 'E']:
-        return 0
-    elif value == ['A', 'B'] or value == ['B', 'A']:
-        return 1
-    elif value == ['B','C'] or value == ['C','B']:
-        return 2
-    elif value == ['C','D'] or value == ['D','C']:
-        return 3
-    elif value == ['D','E'] or value == ['E','D']:
-        return 4
-    elif value == ['E','A'] or value == ['A','E']:
-        return 5
-    elif value == ['A','C'] or value == ['C','A']:
-        return 6
-    elif value == ['C','E'] or value == ['E','C']:
-        return 7
-    elif value == ['B','E'] or value == ['E','B']:
-        return 8
-    elif value == ['B','D'] or value == ['D','B']:
-        return 9
-    elif value == ['A','D'] or value == ['D','A']:
-        return 10
+values = {
+    "AA": 0,
+    "BB": 0,
+    "CC": 0,
+    "DD": 0,
+    "EE": 0,
+    "AB": 1,
+    "BA": 1,
+    "BC": 2,
+    "CB": 2,
+    "CD": 3,
+    "DC": 3,
+    "DE": 4,
+    "ED": 4,
+    "EA": 5,
+    "AE": 5,
+    "AC": 6,
+    "CA": 6,
+    "CE": 7,
+    "EC": 7,
+    "BE": 8,
+    "EB": 8,
+    "BD": 9,
+    "DB": 9,
+    "AD": 10,
+    "DA": 10
+}
+
+
+def verifyValues(value:list):
+    alter_value = "".join(str(x) for x in value)
+    for i in values:
+        if alter_value == i:
+            return values[i]
 
 def pairwise(iterable):
     # pairwise('ABCDE') --> AB BC CD DE
---
+++ teste2.py
@@ -0,0 +1,31 @@
+values = {
+    "AA": 0,
+    "BB": 0,
+    "CC": 0,
+    "DD": 0,
+    "EE": 0,
+    "AB": 1,
+    "BA": 1,
+    "BC": 2,
+    "CB": 2,
+    "CD": 3,
+    "DC": 3,
+    "DE": 4,
+    "ED": 4,
+    "EA": 5,
+    "AE": 5,
+    "AC": 6,
+    "CA": 6,
+    "CE": 7,
+    "EC": 7,
+    "BE": 8,
+    "EB": 8,
+    "BD": 9,
+    "DB": 9,
+    "AD": 10,
+    "DA": 10
+}
+
+for i in values:
+    print(i)
+    print(values[i])
